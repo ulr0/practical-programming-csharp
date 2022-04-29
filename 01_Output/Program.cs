7aae7382058b28cdbb437ad5770d163daefd5bda
ulr0 <ulr0xb@gmail.com>
2022-04-29 17:06:27 +0900

    01_HelloWorld

---
+++ 01_Output/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HellowWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello, World!");
+
+        // 줄바꿈 없이 다음 출력 : Write
+        // 출력 후 줄바꿈 : WriteLine
+        Console.Write("Hello, ");
+        Console.WriteLine("World!");
+        Console.WriteLine("Hiroo");
+        }
+    }
+}
