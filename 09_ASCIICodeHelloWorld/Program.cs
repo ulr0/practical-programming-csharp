ce816efb903eef1af2be5e630d6dba93ef4aa397
ulr0 <ulr0xb@gmail.com>
2022-05-16 21:34:01 +0900

    09_ASCIICodeHellorWorld

---
+++ 09_ASCIICodeHelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ASCIICodeHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+        }
+    }
+}
