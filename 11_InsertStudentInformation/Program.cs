2bb72836dfb489c884205db7f5ba5c4621f6f2e2
ulr0 <ulr0xb@gmail.com>
2022-05-27 22:41:16 +0900

    11_InsertStudentInformation

---
+++ 11_InsertStudentInformation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InsertStudentInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's fill out student information");
+            
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Student Number: ");
+            string studentNumberString = Console.ReadLine();
+            int studentNumber = int.Parse(studentNumberString);
+
+            Console.Write("Grade: ");
+            string gradeString = Console.ReadLine();
+            int grade = int.Parse(gradeString);
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine($"{name}\n{studentNumber}\n{grade}");
+        }
+    }
+}
