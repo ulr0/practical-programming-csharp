c1e13b33ba11f901f0c1da90bcc68e2457007786
ulr0 <ulr0xb@gmail.com>
2022-05-27 21:52:07 +0900

    10_StringInterpolation

---
+++ 10_StringInterpolation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 99;
+            int num2 = 76;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+            Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, num1 + num2);
+            Console.WriteLine($"The sum of {num1} and {num2} is {num1 + num2}");
+
+            Console.WriteLine("\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+            Console.WriteLine(@"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
+
+            Console.WriteLine("{0, -11} + {1, 11} =  {2}", num1, num2, num1 + num2);
+            Console.WriteLine("{0, -11} - {1, 11} =  {2}", num1, num2, num1 - num2);
+            Console.WriteLine("{0, -11} * {1, 11} =  {2}", num1, num2, num1 * num2);
+            Console.WriteLine("{0, -11} / {1, 11} =  {2:f4}", num1, num2, num1 / (float)num2);
+            Console.WriteLine("{0, -11} % {1, 11} =  {2}", num1, num2, num1 % num2);
+        }
+    }
+}
