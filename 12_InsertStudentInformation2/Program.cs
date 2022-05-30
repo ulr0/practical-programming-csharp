d32a040574edd12cdd45ea23ac8feb9ea6e3a9f9
ulr0 <ulr0xb@gmail.com>
2022-05-30 20:06:19 +0900

    12_InsertStudentInformation2

---
+++ 12_InsertStudentInformation2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InsertStudentInformation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's fill out student grade");
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Grade: ");
+            string gradeString = Console.ReadLine();
+            int grade = int.Parse(gradeString);
+
+            if (grade >= 90)
+            {
+                Console.Write("Excellent!");
+            }
+            else if (grade >= 80)
+            {
+                Console.Write("Good!");
+            }
+            else if (grade >= 70)
+            {
+                Console.Write("Meh...");
+            }
+            else if (grade >= 50)
+            {
+                Console.Write("Are you even trying?");
+            }
+            else
+            {
+                Console.Write("You have no talent...");
+            }
+        }
+    }
+}
