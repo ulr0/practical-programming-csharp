2b2e21185261d9ca5ea843d3955b0ce12c3a149a
ulr0 <ulr0xb@gmail.com>
2022-06-02 17:11:07 +0900

    15_CalculatorWithSwitchStatement

---
+++ 15_CalculatorWithSwitchStatement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculatorWithSwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("num1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("num2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write("operation (+, -, *, /): ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    break;
+
+                case "-":
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+
+                case "*":
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+
+                case "/":
+                    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                    break;
+
+                default:
+                    Console.WriteLine($"You entered a wrong operator: {operation}");
+                    break;
+            }
+        }
+    }
+}
