4b9dc7c5854e1f497f76810ce398215997247d55
ulr0 <ulr0xb@gmail.com>
2022-06-04 18:36:30 +0900

    17_Sum(for loop)

---
+++ 17_Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX = 100;
+
+            Console.WriteLine($"sum of 1 - {MAX}");
+
+            int sum = 0;
+            // i를 1 증가시킨 후 대입하지 않기 때문에 i++ 이든, ++i 이든 차이 없음
+            for (int i = 0; i < MAX; ++i)
+            {
+                sum += (i + 1);
+            }
+            
+            // 또는
+            // for (int i = 1; i <= MAX; ++i)
+            // {
+            //     sum += i;
+            // }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
