7e33779ffa246dfe3aa8bc952a252a9e11e44aa9
ulr0 <ulr0xb@gmail.com>
2022-06-05 19:35:20 +0900

    18_InfiniteWhileLoop

---
+++ 18_InfiniteWhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InfiniteWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Input Number: ");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input ==0)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Number in hex: 0x{input:x}");
+            }
+        }
+    }
+}
