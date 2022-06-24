280dbbf960873e8f323e32a2af5a556707bfd4d6
ulr0 <ulr0xb@gmail.com>
2022-06-24 21:09:19 +0900

    30_DefaultParameter

---
+++ 30_DefaultParameter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefaultParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bar("hi");
+            Bar("hi", "roo");
+            Bar("hi", "roo", "bye");
+        }
+
+        static void Bar(string s1, string s2 = "", string s3 = "")
+        {
+            Console.WriteLine($"{s1}, {s2}, {s3}");
+        }
+    }
+}
