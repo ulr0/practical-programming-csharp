58c1072eac9769a269a775d9d92c01cbaeb73e17
ulr0 <ulr0xb@gmail.com>
2022-07-02 17:56:42 +0900

    36_Dictionary

---
+++ 36_Dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionatyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACITY = 20;
+            
+            List<int> list = new List<int>(CAPACITY);
+
+            Random random = new Random();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                list.Add(random.Next(0, 10));
+            }
+
+            Console.WriteLine($"[ {string.Join(",", list)} ]");
+
+            Dictionary<int, bool> dictionary = new Dictionary<int, bool>();
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                if (dictionary.ContainsKey(list[i]))
+                {
+                    list.Remove(list[i]);
+                }
+                else
+                {
+                    dictionary.Add(list[i], true);
+                }
+            }
+
+            Console.WriteLine($"[ {string.Join(",", list)} ]");
+        }
+    }
+}
