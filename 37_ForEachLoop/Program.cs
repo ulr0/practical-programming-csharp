ac7fa9c39e85673acd90d941123ed10a35a810da
ulr0 <ulr0xb@gmail.com>
2022-07-03 23:12:21 +0900

    37_ForEachLoop

---
+++ 37_ForEachLoop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                list.Add(random.Next(0, 10));
+            }
+
+            int sum = 0;
+
+            foreach (int i in list)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine("-------------------------------");
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>
+            {
+                { "key1", 1 },
+                { "key2", 2 },
+                { "key3", 3 },
+                { "key4", 4 },
+                { "key5", 5 },
+                { "key6", 6 },
+            };
+
+            foreach(KeyValuePair<string, int> entry in dictionary)
+            {
+                Console.WriteLine($"{entry.key}: {entry.value}");
+            }
+        }
+    }
+}
