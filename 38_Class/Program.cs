be34c3a4dc832201ec8d317cc9d063637ab5cdfa
ulr0 <ulr0xb@gmail.com>
2022-07-09 23:26:38 +0900

    38_Class

---
+++ 38_Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior warrior = new Warrior("David");
+
+            warrior.Introduce();
+            warrior.GetStatus();
+
+
+            // 컴파일 오류
+            // Name 프로퍼티는 private set을 사용하기 때문에
+            // Warrior 클래스 밖에서 설정(set)할 수 없다.
+            // warrior.Name = "Peter";
+
+            warrior.SwordStrike();
+            warrior.UseWhirlwind();
+            warrior.UseWhirlwind();
+            warrior.UseWhirlwind();
+            warrior.Rest();
+
+            warrior.GetStatus();
+
+            // Health 프로퍼티 public set
+            warrior.Health -= 200;
+
+            // 컴파일 오류
+            // mHealth 멤버 변수는 private
+            // warrior.mHealth = 5000;
+
+            warrior.GetStatus();
+        }
+    }
+}
---
+++ 38_Class/Warrior.cs
@@ -0,0 +1,137 @@
+using System;
+
+namespace Class
+{
+    public class Warrior
+    {   
+        // 멤버 변수
+        private const int MAX_HEALTH = 500;
+        private const int MAX_MANA = 50;
+        private const int MAX_STAMINA = 200;
+
+        private int mHealth = MAX_HEALTH;
+        private int mMana = MAX_MANA;
+        private int mStamina = MAX_STAMINA;
+
+        // 생성자
+        public Warrior(string name)
+        {
+            Name = name;
+        }
+
+        // 프로퍼티
+        public string Name { get; private set; }
+
+        public int Health
+        {
+            get
+            {
+                return mHealth;
+            }
+            set
+            {
+                mHealth = value;
+
+                if (mHealth < 0)
+                {
+                    mHealth = 0;
+                }
+
+                if (mHealth > MAX_HEALTH)
+                {
+                    mHealth = MAX_HEALTH;
+                }
+            }
+        }
+
+        public int Mana
+        {
+            get
+            {
+                return mMana;
+            }
+            set
+            {
+                mMana = value;
+
+                if (mMana < 0)
+                {
+                    mMana = 0;
+                }
+
+                if (mMana > MAX_MANA)
+                {
+                    mMana = MAX_MANA;
+                }
+            }
+        }
+
+        public int Stamina
+        {
+            get
+            {
+                return mStamina;
+            }
+            set
+            {
+                mStamina = value;
+
+                if (mStamina < 0)
+                {
+                    mStamina = 0;
+                }
+
+                if (mStamina > MAX_STAMINA)
+                {
+                    mStamina = MAX_STAMINA;
+                }
+            }
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"I'm {Name}!");
+        }
+
+        public void UseWhirlwind()
+        {
+            if (Mana < 5 || Stamina < 70)
+            {
+                Console.WriteLine($"{Name} can't use Whirlwind!");
+                return;
+            }
+
+            Mana -= 5;
+            Stamina -= 70;
+
+            Console.WriteLine($"{Name} used Whirlwind!");
+        }
+
+        public void SwordStrike()
+        {
+            if (Stamina < 20)
+            {
+                Console.WriteLine($"{Name} can't use SwordStrike!");
+                return;
+            }
+
+            Stamina -= 20;
+
+            Console.WriteLine($"{Name} used SwordStrike!");
+        }
+
+        public void Rest()
+        {
+            Mana += 2;
+            Stamina += 5;
+            Health += 5;
+
+            Console.WriteLine($"{Name} is resting.");
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine($"{Name} - Health: {Health} / Mana: {Mana} / Stamina: {Stamina}");
+        }
+    }
+}
