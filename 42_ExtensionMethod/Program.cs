8a132a762b539a83456ddfac44bf103d67966d3d
ulr0 <ulr0xb@gmail.com>
2022-07-12 22:55:03 +0900

    42_ExtensionMethod

---
+++ 42_ExtensionMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string HELLO_WORLD = "Hello World!";
+
+            string reversed = HELLO_WORLD.Reverse();
+
+            Console.WriteLine(reversed);
+
+            string reversed2 = HELLO_WORLD.Reverse(3, 8);
+
+            Console.WriteLine(reversed2);
+        }
+    }
+}
