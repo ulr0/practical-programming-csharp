c5940db68afe0d7d0dbf35ff1e23e6c9b6a14a45
ulr0 <ulr0xb@gmail.com>
2022-07-16 22:17:21 +0900

    44_Struct

---
+++ 44_Struct/CartItem.cs
@@ -0,0 +1,12 @@
+namespace Struct
+{
+    // class X struct O
+    public struct CartItem
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public uint Quantity { get; set; }
+    }
+}
---
+++ 44_Struct/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CartItem cartItem = new CartItem
+            {
+                Name = "Hello World! Programming Book",
+                Price = 10.99m,
+                Quantity = 5
+            };
+
+            // CartItem은 구조체. 즉, 값형.
+            // 함수 인자로 원본이 아닌 복사본 전달.
+            ModifyCartItem(cartItem);
+
+            // 원본 변화 없음.
+            Console.WriteLine($"Name: {cartItem.Name}");
+            Console.WriteLine($"Price: {cartItem.Price}");
+            Console.WriteLine($"Quantity: {cartItem.Quantity}");
+        }
+
+        public static void ModifyCartItem(CartItem cartItem)
+        {
+            cartItem.Name = "Houw to suck at programming";
+            cartItem.Price = 0.99m;
+            cartItem.Quantity = 1;
+        }
+    }
+}
